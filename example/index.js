b00cf4528d5d51bf4995484a636ced9ae01b6cb5
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2025-10-08 15:23:28 +0800

    feat: ios works

--- example/index.js
+++ example/index.js
@@ -1,5 +1,8 @@
 import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
+import { uniffiInitAsync } from 'my-test-library';
 
-AppRegistry.registerComponent(appName, () => App);
+uniffiInitAsync().then(() => {
+  AppRegistry.registerComponent(appName, () => App);
+});
